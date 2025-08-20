1e1237509a0e39b3cb290081c6c53e192b53e6a6
LeonardoReichardt <engsoft.leonardo@gmail.com>
2025-08-20 18:26:24 -0300

    layout cards and tags

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,13 +5,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Pixel Positions</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Hanken+Grotesk:wght@400;500;600&display=swap" rel="stylesheet">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
-<body class="bg-black text-white">
+<body class="bg-black text-white font-hanken-grotesk">
     <div class="px-10">
         <nav class="flex justify-between items-center py-4 border-b border-white/10">
             <div>
-                <a href="">
+                <a href="/">
                     <img src="{{ Vite::asset('resources/images/logo.svg')}}" alt="">
                 </a>
             </div>
@@ -27,7 +30,7 @@
                 <a href="#">Post a Job</a>
             </div>
         </nav>
-        <main class="mt-10 max-w-[986px]">
+        <main class="mt-10 max-w-[1920px]">
             {{ $slot }}
         </main>
 
