429eb4c39bf6e05d52cbe6c3dcf35489e3989bb9
LeonardoReichardt <engsoft.leonardo@gmail.com>
2025-08-22 20:34:55 -0300

    create seeders

--- resources/views/components/job-card-wide.blade.php
+++ resources/views/components/job-card-wide.blade.php
@@ -1,18 +1,20 @@
+@props(['job'])
+
 <x-panel class="flex gap-x-6 ">
     <div>
         <x-employer-logo />
     </div>
 
     <div class="flex-1 flex flex-col">
-        <a class="self-start text-sm text-gray-400 transation-colors duration-300">Laracasts</a>
+        <a class="self-start text-sm text-gray-400 transation-colors duration-300">{{ $job->employer->name }}</a>
 
-        <h3 class="font-bold text-xl mt-3 group-hover:text-blue-800 text-xl font-bold">Video Producer</h3>
-        <p class="text-sm text-gray-400 mt-auto">Full Time - From $60,000</p>
+        <h3 class="font-bold text-xl mt-3 group-hover:text-blue-800 text-xl font-bold">{{ $job->title }}</h3>
+        <p class="text-sm text-gray-400 mt-auto">{{ $job->salary }}</p>
     </div>
 
     <div>
-        <x-tag>Tag</x-tag>
-        <x-tag>Tag</x-tag>
-        <x-tag>Tag</x-tag>
+        @foreach ($job->tags as $tag)
+            <x-tag :$tag />
+        @endforeach
     </div>
 </x-panel>
