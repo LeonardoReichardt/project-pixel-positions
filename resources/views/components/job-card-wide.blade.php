da18af28e5482b75cc8917e9a69cc88a0d581e3e
LeonardoReichardt <engsoft.leonardo@gmail.com>
2025-08-23 10:57:25 -0300

    create new job

--- resources/views/components/job-card-wide.blade.php
+++ resources/views/components/job-card-wide.blade.php
@@ -8,7 +8,11 @@
     <div class="flex-1 flex flex-col">
         <a class="self-start text-sm text-gray-400 transation-colors duration-300">{{ $job->employer->name }}</a>
 
-        <h3 class="font-bold text-xl mt-3 group-hover:text-blue-800 text-xl font-bold">{{ $job->title }}</h3>
+        <h3 class="font-bold text-xl mt-3 group-hover:text-blue-800 text-xl font-bold">
+            <a href="{{ $job->url }}" target="_blank">
+                {{ $job->title }}
+            </a>
+        </h3>
         <p class="text-sm text-gray-400 mt-auto">{{ $job->salary }}</p>
     </div>
 
